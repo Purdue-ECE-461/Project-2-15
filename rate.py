d990cdd32ba1082dd28a1a9c2afff5577cdd3c19
wxjdsr <wxjdsr@126.com>
2021-12-08 19:43:54 -0400

    test if deploy works

---
+++ rate.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('/Project_2_starter/project-1-project-1-14/src')
+
+# Original Description:
+# The “rate” option should provide the net score and sub-scores from Project 1,
+# to inform prospective users of the quality of the module.
+# It should also include one new metric: The fraction of its dependencies
+# that are pinned to at least a specific major+minor version,
+# e.g. version 2.3.X of a package.
+# (If there are zero dependencies, they should receive a 1.0 rating.
+# If there are two dependencies, one pinned to this degree,
+# then they should receive a ½ = 0.5 rating).
+
+run()
